a40d3bf7f91a1afff8215236b5168aed2116dac8
zheng851228 <158807318+zheng851228@users.noreply.github.com>
2026-04-08 06:43:25 +0800

    更新 config.js

--- js/config.js
+++ js/config.js
@@ -1,26 +1,27 @@
-'use strict';
-const CWA_KEY = 'CWA-57962C34-72D2-446D-98D4-63B80BD8F9FB';
-const C = {
-  MAP: { center: [23.8, 121.0], zoom: 7, cityZoom: 13, flyDur: 1.2 },
-  TILES: {
-    dark: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-    light: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-  },
-  ROUTE_RADIUS_KM: 1.0,
-  ROUTE_THIN_STEP: 3,
-  COLS: {
-    dark: { free: '#22c55e', slow: '#f59e0b', bad: '#ef4444' },
-    light: { free: '#16a34a', slow: '#d97706', bad: '#dc2626' }
-  },
-  SRC: {
-    tdx: { lbl: 'TDX平台', col: '#3b82f6' },
-    city: { lbl: '縣市政府', col: '#8b5cf6' },
-    yt: { lbl: 'YouTube', col: '#ff0000' }
-  },
-  ST: {
-    free: { lbl: '順暢', p: 'bg-free', c: 'c-free' },
-    slow: { lbl: '緩慢', p: 'bg-slow', c: 'c-slow' },
-    bad: { lbl: '壅塞', p: 'bg-bad', c: 'c-bad' }
-  },
-  STAT_LBL: { free: '順暢路段', slow: '緩慢路段', bad: '壅塞路段' }
+(function() {
+‘use strict’;
+
+window.Config = {
+CWA_KEY: ‘CWA-57962C34-72D2-446D-98D4-63B80BD8F9FB’,
+MAP_CENTER: [23.9, 121.0],
+MAP_ZOOM: 7,
+ROUTE_FILTER_KM: 1.0,
+SIMPLIFY_STEP: 5,
+TILE_DARK: ‘https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png’,
+TILE_LIGHT: ‘https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png’,
+TILE_ATTR: ‘© OpenStreetMap © CARTO’,
+STATUS_COLOR: {
+smooth:  ‘#3fb950’,
+slow:    ‘#d29922’,
+jam:     ‘#da3633’,
+unknown: ‘#8b949e’
+},
+REGIONS: {
+north:   [‘台北市’,‘新北市’,‘基隆市’,‘桃園市’,‘新竹市’,‘新竹縣’,‘宜蘭縣’],
+central: [‘苗栗縣’,‘台中市’,‘彰化縣’,‘南投縣’,‘雲林縣’],
+south:   [‘嘉義市’,‘嘉義縣’,‘台南市’,‘高雄市’,‘屏東縣’],
+east:    [‘花蓮縣’,‘台東縣’],
+island:  [‘澎湖縣’,‘金門縣’,‘連江縣’]
+}
 };
+})();
